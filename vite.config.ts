db04c3c88db6e1d5d9ed7ef81c485866f0d2375c
ngocdungpham <24020094@vnu.edu.vn>
2025-12-04 00:34:37 +0700

    Update base url for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
+  // THÊM DÒNG NÀY VÀO:
+  base: "/Student-Management/", 
 })
